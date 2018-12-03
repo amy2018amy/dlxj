917ff2a9c545eda07e18421b7d842bb19fd66896
az <631252194@qq.com>
2018-12-03 22:07:27 +0800

    Initial commit

---
+++ src/main/java/com/az/dlxj/DlxjApplication.java
@@ -0,0 +1,12 @@
+package com.az.dlxj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlxjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlxjApplication.class, args);
+    }
+}
